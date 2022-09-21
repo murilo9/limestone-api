673e41d7440cc0839fc0fe1af2e4c7fc18e90611
murilo9 <murilohenriquematias@gmail.com>
2022-09-21 15:38:29 -0300

    implemented identity guard

---
+++ src/auth/identity.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from '../users/entities/user.entity';
+import { DatabaseService } from '../database/database.service';
+import { SignInDto } from './dto/signin.dto';
+import { verify } from 'jsonwebtoken';
+
+export class IdentityGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<{
+      body: SignInDto;
+      user?: User;
+      headers: { access_token: string };
+    }>();
+    const { access_token } = request.headers;
+    try {
+      const user = verify(access_token, 'secret') as User;
+      console.log(user);
+      request.user = user;
+      return true;
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException('Could not validate access_token.');
+    }
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,14 @@ import {
   Delete,
   Put,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { SignInDto } from '../auth/dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { IdentityGuard } from 'src/auth/identity.guard';
 
 @Controller()
 export class UsersController {
@@ -29,6 +31,7 @@ export class UsersController {
     return this.usersService.verify(id);
   }
 
+  @UseGuards(IdentityGuard)
   @Get('/users')
   getAll() {
     return this.usersService.getAll();
