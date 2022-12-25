e8454b291ff495e3c2b3bb6c6126c4d4c7e83ad9
murilo9 <murilohenriquematias@gmail.com>
2022-12-25 16:08:14 -0400

    better todo comments

--- src/cards/cards.service.spec.ts
+++ src/cards/cards.service.spec.ts
@@ -21,25 +21,49 @@ describe('CardsService', () => {
   });
 
   describe('method: create', () => {
-    it('should return the created card');
-    it('should insert the created card in the database');
+    it('should return the created card', () => {
+      /* todo */
+    });
+    it('should insert the created card in the database', () => {
+      /* todo */
+    });
   });
   describe('method: update', () => {
-    it('should return the updated card');
-    it('should update the card in the database');
+    it('should return the updated card', () => {
+      /* todo */
+    });
+    it('should update the card in the database', () => {
+      /* todo */
+    });
   });
   describe('method: get', () => {
-    it('should return the card when it exists');
-    it('should return null when the card does not exist');
-    it('should search for the card in the database');
+    it('should return the card when it exists', () => {
+      /* todo */
+    });
+    it('should return null when the card does not exist', () => {
+      /* todo */
+    });
+    it('should search for the card in the database', () => {
+      /* todo */
+    });
   });
   describe('method: getByColumn', () => {
-    it('should return an array of cards when cards exist');
-    it('should return an empty array when no cards exist with column id');
-    it('should return an empty array when column does not exist');
+    it('should return an array of cards when cards exist', () => {
+      /* todo */
+    });
+    it('should return an empty array when no cards exist with column id', () => {
+      /* todo */
+    });
+    it('should return an empty array when column does not exist', () => {
+      /* todo */
+    });
   });
   describe('method: delete', () => {
-    it('should return a scuccess message');
-    it('should delete the card in the database');
+    it('should return a scuccess message', () => {
+      /* todo */
+    });
+    it('should delete the card in the database', () => {
+      /* todo */
+    });
   });
 });
