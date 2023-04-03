427d7ab2e42d69ee8e8670797ec3c65cc70d2651
murilo9 <murilohenriquematias@gmail.com>
2023-04-02 21:21:03 -0300

    fixed cors config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://limestone.app.br'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   });
   await app.listen(8080);
 }
