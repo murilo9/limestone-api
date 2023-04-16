e74b22f8fe9e8433931214312a71c660cd92b4f6
murilo9 <murilohenriquematias@gmail.com>
2023-04-16 13:30:28 -0300

    fixed ObjectId user id at identity guard

--- src/auth/identity.guard.ts
+++ src/auth/identity.guard.ts
@@ -8,6 +8,7 @@ import { User } from '../users/entities/user.entity';
 import { DatabaseService } from '../database/database.service';
 import { SignInDto } from './dto/signin.dto';
 import { verify } from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
 
 export class IdentityGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -19,7 +20,11 @@ export class IdentityGuard implements CanActivate {
     const { authorization } = request.headers;
     try {
       const user = verify(authorization, 'SECRET') as User;
-      request.user = user;
+      // user _id actually comes as string, so it must be converted to ObjectId here
+      request.user = {
+        ...user,
+        _id: new ObjectId(user._id),
+      };
       return true;
     } catch (error) {
       console.log(error);
